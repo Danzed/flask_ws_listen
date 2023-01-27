ded4195fe197b26c7a8bbd45c2a85fe6efee6935
Daniel Hernandez <df.hernandez@hotmail.cl>
2023-01-27 11:18:23 -0300

    init

---
+++ init.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from requests import get
+
+app = Flask('__main__')
+SITE_NAME = 'https://ifconfig.co/'
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def proxy(path):
+  return get(f'{SITE_NAME}{path}').content
+app.run(host='0.0.0.0', port=8080)
